6762c14beceae4eff5d4e4015b61b9ec9b65c5cd
cjycathy <827221382@qq.com>
2021-01-15 18:26:53 +0800

    首次上传

---
+++ src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">
+        <h1>test</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style lang="less" scoped>
+    .test{
+        color: red;
+    }
+</style>
